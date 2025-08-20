693ac0ba90c01f22514f719e08803062121cbca0
Paul O’Mahony <paul@atyos.io>
2025-08-20 12:23:29 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from utils import print_n_lines
+
+if __name__ == "__main__":
+    print_n_lines(10)
---
+++ utils.py
@@ -0,0 +1,6 @@
+def print_line():
+    print(50*"=")
+
+def print_n_lines(n: int):
+    for i in range(n):
+        print_line()
